19c1627e7b23710106074306863da8f9a79e813f
Rzftry <muhamadrazif1@gmail.com>
2025-10-29 13:09:00 +0800

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'myott-v1';
+const FILES_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/assets/style.css',
+  '/assets/logo.png'
+];
+
+self.addEventListener('install', (evt) => {
+  evt.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (evt) => {
+  evt.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (evt) => {
+  if (evt.request.method !== 'GET') return;
+  evt.respondWith(
+    caches.match(evt.request).then((res) => res || fetch(evt.request))
+  );
+});
